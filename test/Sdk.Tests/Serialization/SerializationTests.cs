61557f4cf9a7b90d1ee7d7683c90fb408e9783c3
Ben Foster <ben.foster@truelayer.com>
2021-05-20 05:38:45 +0100

    Record serialization tests

---
+++ test/Sdk.Tests/Serialization/SerializationTests.cs
@@ -0,0 +1,25 @@
+using Shouldly;
+using TrueLayer.Serialization;
+using Xunit;
+
+namespace TrueLayer.Sdk.Tests.Serialization
+{
+    public class SerializationTests
+    {
+        private readonly JsonSerializer _serializer = new();
+        
+        [Fact]
+        public void Can_handle_nullable_fields_with_record_constructors()
+        {
+            TestRecord obj = new ("Required", null);
+            string json = _serializer.Serialize(obj);    
+
+            TestRecord deserialized = _serializer.Deserialize(json, typeof(TestRecord)) as TestRecord;
+            deserialized.ShouldNotBeNull();
+            deserialized.RequiredField.ShouldBe(obj.RequiredField);
+            deserialized.OptionalField.ShouldBe(obj.OptionalField);
+        }
+
+        record TestRecord(string RequiredField, string OptionalField);
+    }
+}
