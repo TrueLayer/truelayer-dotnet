b2a784c415883238b8616baf0741014b74169162
Ben Foster <ben.foster@truelayer.com>
2021-10-16 23:02:37 +0100

    response type for empty responses

--- src/TrueLayer/ApiResponse.cs
+++ src/TrueLayer/ApiResponse.cs
@@ -2,7 +2,7 @@ using System.Net;
 
 namespace TrueLayer
 {
-    public class ApiResponse<TData>
+    public class ApiResponse
     {
         internal ApiResponse(HttpStatusCode statusCode, string? traceId)
         {
@@ -17,22 +17,10 @@ namespace TrueLayer
             Problem = problemDetails.NotNull(nameof(problemDetails));
         }
 
-        internal ApiResponse(TData data, HttpStatusCode statusCode, string? traceId)
-        {
-            Data = data.NotNull(nameof(data));
-            StatusCode = statusCode;
-            TraceId = traceId;
-        }
-
         public HttpStatusCode StatusCode { get; }
         public string? TraceId { get; }
 
-        // TODO NotNullWhen 
-        public TData? Data { get; }
-
         public ProblemDetails? Problem { get; }
-
-        public static implicit operator TData?(ApiResponse<TData> response) => response.Data;
 
         public bool IsSuccessful => (int)StatusCode is (>= 200 and <= 299);
     }
---
+++ src/TrueLayer/ApiResponseOfT.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+
+namespace TrueLayer
+{
+    public class ApiResponse<TData> : ApiResponse
+    {
+        internal ApiResponse(HttpStatusCode statusCode, string? traceId)
+            : base(statusCode, traceId)
+        {
+
+        }
+
+        internal ApiResponse(ProblemDetails problemDetails, HttpStatusCode statusCode, string? traceId)
+            : base(problemDetails, statusCode, traceId)
+        {
+        }
+
+        internal ApiResponse(TData data, HttpStatusCode statusCode, string? traceId)
+            : base(statusCode, traceId)
+        {
+            Data = data.NotNull(nameof(data));
+        }
+
+        //[MemberNotNullWhen(true, nameof(ApiResponse.IsSuccessful))]
+        public TData? Data { get; }
+
+        public static implicit operator TData?(ApiResponse<TData> response) => response.Data;
+    }
+}
