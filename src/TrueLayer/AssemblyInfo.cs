2b934e7a891ed356ce9ff1d0fd031e8fbf515358
Ben Foster <ben.foster@truelayer.com>
2021-10-18 15:08:19 +0100

    Fixed up benchmarks

--- src/TrueLayer/AssemblyInfo.cs
+++ src/TrueLayer/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleToAttribute("TrueLayer.Tests")]
+[assembly: InternalsVisibleToAttribute("TrueLayer.Benchmarks")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
