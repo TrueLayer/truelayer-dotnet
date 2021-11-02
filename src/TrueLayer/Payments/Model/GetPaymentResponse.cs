db7ec3bc46ecf705006a8e84ae4eb23c61b4f738
David Hayden <david.hayden@truelayer.com>
2021-11-02 21:39:43 +0000

    Update Statuses

--- src/TrueLayer/Payments/Model/GetPaymentResponse.cs
+++ src/TrueLayer/Payments/Model/GetPaymentResponse.cs
@@ -74,51 +74,39 @@ namespace TrueLayer.Payments.Model
         /// <summary>
         /// Represents a payment that has been authorized by the end user
         /// </summary>
-        /// <param name="AuthorizedAt">The date and time the payment was authorized</param>
         /// <returns></returns>
         [JsonDiscriminator("authorized")]
-        public record Authorized(DateTime AuthorizedAt) : PaymentDetails;
-
-        /// <summary>
-        /// Represents a payment that failed to be authorized
-        /// </summary>
-        /// <param name="FailedAt">The date and time the authorization failed</param>
-        /// <param name="FailureReason">The reason for failure</param>
-        /// <returns></returns>
-        [JsonDiscriminator("authorization_failed")]
-        public record AuthorizationFailed(DateTime FailedAt, string FailureReason) : PaymentDetails;
+        public record Authorized() : PaymentDetails;
 
         /// <summary>
         /// Represents a payment that has succeeded
         /// For open loop payments this state is terminate. For closed-loop payments, wait for Settled.
         /// </summary>
-        /// <param name="AuthorizedAt">The date and time the payment was authorized</param>
         /// <param name="SucceededAt">The date and time the payment succeeded</param>
         /// <param name="SourceOfFunds">Details of the source of funds for the payment</param>
         /// <returns></returns>
-        [JsonDiscriminator("successful")]
-        public record Successful(DateTime AuthorizedAt, DateTime SucceededAt, SourceOfFundsUnion SourceOfFunds) : PaymentDetails;
+        [JsonDiscriminator("succeeded")]
+        public record Succeeded(DateTime SucceededAt, SourceOfFundsUnion SourceOfFunds) : PaymentDetails;
 
         /// <summary>
         /// Represents a payment that has settled
         /// This is a terminate state for closed-loop payments.
         /// </summary>
-        /// <param name="AuthorizedAt">The date and time the payment was authorized</param>
         /// <param name="SucceededAt">The date and time the payment succeeded</param>
         /// <param name="SettledAt">The date and time the payment was settled</param>
         /// <param name="SourceOfFunds">Details of the source of funds for the payment</param>
         /// <returns></returns>
         [JsonDiscriminator("settled")]
-        public record Settled(DateTime AuthorizedAt, DateTime SucceededAt, DateTime SettledAt, SourceOfFundsUnion SourceOfFunds) : PaymentDetails;
+        public record Settled(DateTime SucceededAt, DateTime SettledAt, SourceOfFundsUnion SourceOfFunds) : PaymentDetails;
 
         /// <summary>
         /// Represents an authorized payment that failed to complete. This is a terminate state.
         /// </summary>
-        /// <param name="AuthorizedAt">The date and time the payment was authorized</param>
         /// <param name="FailedAt">The date and time the payment failed</param>
+        /// <param name="FailureStage">The status the payment was in when it failed</param>
         /// <param name="FailureReason">The reason for failure</param>
         /// <returns></returns>
         [JsonDiscriminator("failed")]
-        public record Failed(DateTime AuthorizedAt, DateTime FailedAt, string FailureReason) : PaymentDetails;
+        public record Failed(DateTime FailedAt, string FailureStage, string FailureReason) : PaymentDetails;
     }
 }
