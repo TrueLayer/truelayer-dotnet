9d96db053d4ff60d2c864069280d94585089e5c9
tl-antonio-valentini <antonio.valentini@truelayer.com>
2021-11-03 18:21:17 +0100

    update sample app with merchants

---
+++ examples/MvcExample/Controllers/MerchantsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TrueLayer;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcExample.Controllers
+{
+    public class MerchantsController : Controller
+    {
+        private readonly ITrueLayerClient _truelayer;
+        private readonly ILogger<MerchantsController> _logger;
+
+        public MerchantsController(ITrueLayerClient truelayer, ILogger<MerchantsController> logger)
+        {
+            _truelayer = truelayer;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var apiResponse = await _truelayer.Merchants.ListMerchants();
+
+            if (!apiResponse.IsSuccessful)
+            {
+                if (apiResponse.Problem?.Errors != null)
+                {
+                    foreach (var error in apiResponse.Problem.Errors)
+                    {
+                        ModelState.AddModelError("", $"{error.Key}: {error.Value?.FirstOrDefault()}");
+                    }
+                }
+                
+                ModelState.AddModelError("", "Failed to gather merchants list");
+                return View();
+            }
+            
+            return View(apiResponse.Data?.items);
+        }
+    }
+}
