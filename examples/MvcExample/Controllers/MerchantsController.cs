5b8c36139a7d40ca006ba22aea75dc1c3579f33e
tl-antonio-valentini <antonio.valentini@truelayer.com>
2021-11-04 13:07:46 +0100

    merchant account details page

--- examples/MvcExample/Controllers/MerchantsController.cs
+++ examples/MvcExample/Controllers/MerchantsController.cs
@@ -38,7 +38,30 @@ namespace MvcExample.Controllers
             }
 
             return View();
+        }
 
+        public async Task<IActionResult> Details(string id)
+        {
+            var apiResponse = await _truelayer.Merchants.GetMerchant(id);
+
+            if (apiResponse.IsSuccessful)
+            {
+                return View(apiResponse.Data);
+            }
+
+            _logger.LogError("Get merchant details failed with status code {StatusCode}", apiResponse.StatusCode);
+
+            if (apiResponse.Problem != null)
+            {
+                var problem = apiResponse.Problem;
+                ModelState.AddModelError("", $"{problem.Title}, {problem.Detail}");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Failed to gather merchant details");
+            }
+
+            return View();
         }
     }
 }
