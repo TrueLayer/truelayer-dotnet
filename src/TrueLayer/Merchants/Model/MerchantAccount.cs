2bf6f186953a4800cd66b8e83012711b2d21d416
tl-antonio-valentini <antonio.valentini@truelayer.com>
2021-11-04 12:44:32 +0100

    use SchemeIdentifier for merchant account

--- src/TrueLayer/Merchants/Model/MerchantAccount.cs
+++ src/TrueLayer/Merchants/Model/MerchantAccount.cs
@@ -1,11 +1,20 @@
+using OneOf;
+using SchemeIdentifier = TrueLayer.Payments.Model.SchemeIdentifier;
 using System.Collections.Generic;
 
 namespace TrueLayer.Merchants.Model
 {
+    using SchemeIdentifiersUnion = OneOf<
+        SchemeIdentifier.SortCodeAccountNumber,
+        SchemeIdentifier.Bban,
+        SchemeIdentifier.Iban,
+        SchemeIdentifier.Nrb
+    >;
+
     public record MerchantAccount(
         string Id, 
         string Currency, 
-        IEnumerable<object> SchemeIdentifiers,
+        IEnumerable<SchemeIdentifiersUnion> SchemeIdentifiers,
         long AvailableBalanceInMinor, 
         long CurrentBalanceInMinor, 
         string AccountHolderName);
