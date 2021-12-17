bf3e7872134739dbfc3fa3cb6ebcc97d3a0eafe7
Mauro Franchi <mauro.franchi@truelayer.com>
2021-12-17 18:45:17 +0100

    ISSUE-88 - Implemented PayoutsApi in TrueLayerClient

--- src/TrueLayer/TrueLayerClient.cs
+++ src/TrueLayer/TrueLayerClient.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Options;
 using TrueLayer.Auth;
 using TrueLayer.Payments;
 using TrueLayer.MerchantAccounts;
+using TrueLayer.Payouts;
 
 namespace TrueLayer
 {
@@ -10,6 +11,7 @@ namespace TrueLayer
     {
         // APIs that require specific configuration should be lazily initialised
         private readonly Lazy<IPaymentsApi> _payments;
+        private readonly Lazy<IPayoutsApi> _payouts;
         private readonly Lazy<IMerchantAccountsApi> _merchants;
 
         public TrueLayerClient(IApiClient apiClient, IOptions<TrueLayerOptions> options)
@@ -19,11 +21,13 @@ namespace TrueLayer
 
             Auth = new AuthApi(apiClient, options.Value);
             _payments = new(() => new PaymentsApi(apiClient, Auth, options.Value));
+            _payouts = new(() => new PayoutsApi(apiClient, Auth, options.Value));
             _merchants = new(() => new MerchantAccountsApi(apiClient, Auth, options.Value));
         }
 
         public IAuthApi Auth { get; }
         public IPaymentsApi Payments => _payments.Value;
+        public IPayoutsApi Payouts => _payouts.Value;
         public IMerchantAccountsApi MerchantAccounts => _merchants.Value;
     }
 }
