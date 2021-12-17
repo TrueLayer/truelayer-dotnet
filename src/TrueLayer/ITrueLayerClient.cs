ec753dd796c664ee413594d515711aff4f46cd61
Mauro Franchi <mauro.franchi@truelayer.com>
2021-12-17 18:08:09 +0100

    ISSUE-88 - Added Payouts to ITrueLayerClient

--- src/TrueLayer/ITrueLayerClient.cs
+++ src/TrueLayer/ITrueLayerClient.cs
@@ -1,6 +1,7 @@
 using TrueLayer.Auth;
 using TrueLayer.Payments;
 using TrueLayer.MerchantAccounts;
+using TrueLayer.Payouts;
 
 namespace TrueLayer
 {
@@ -13,11 +14,16 @@ namespace TrueLayer
         /// Gets the Authorization API resource
         /// </summary>
         IAuthApi Auth { get; }
-        
+
         /// <summary>
         /// Gets the Payments API resource
         /// </summary>
         IPaymentsApi Payments { get; }
+
+        /// <summary>
+        /// Gets the Payouts API resource
+        /// </summary>
+        IPayoutsApi Payouts { get; }
 
         /// <summary>
         /// Gets the Merchant Accounts API resource
