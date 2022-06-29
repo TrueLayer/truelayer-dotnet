6e53d54f1b5669550cbefa780e6fe160dab58d38
Mauro Franchi <mauro.franchi@truelayer.com>
2022-06-29 17:54:43 +0200

    ISSUE-131 - Added RemitterAccount model

---
+++ src/TrueLayer/Payments/Model/RemitterAccount.cs
@@ -0,0 +1,15 @@
+using OneOf;
+
+namespace TrueLayer.Payments.Model
+{
+    using AccountIdentifierUnion = OneOf<
+        AccountIdentifier.SortCodeAccountNumber,
+        AccountIdentifier.Iban>;
+
+    /// <summary>
+    /// Represent a remitter account
+    /// </summary>
+    /// <param name="AccountHolderName">The name of the remitter account holder</param>
+    /// <param name="AccountIdentifier">A unique account identifier for the remitter account</param>
+    public record RemitterAccount(string AccountHolderName, AccountIdentifierUnion AccountIdentifier);
+}
