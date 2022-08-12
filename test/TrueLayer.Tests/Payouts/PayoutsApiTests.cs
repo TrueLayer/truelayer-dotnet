cf484832ade9aa520e2a695cb64b9f6ed29d97a2
tl-antonio-valentini <antonio.valentini@truelayer.com>
2022-08-12 15:50:39 +0200

    Add tests

--- test/TrueLayer.Tests/Payouts/PayoutsApiTests.cs
+++ test/TrueLayer.Tests/Payouts/PayoutsApiTests.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using OneOf;
 using Moq;
 using Shouldly;
 using TrueLayer.Auth;
@@ -34,8 +36,9 @@ namespace TrueLayer.Tests.Payouts
             };
         }
 
-        [Fact]
-        public async Task CreatePayout_Returns_Empty_Response_With_Http_Status_Code_On_Auth_Failure()
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public async Task CreatePayout_Returns_Empty_Response_With_Http_Status_Code_On_Auth_Failure(CreatePayoutRequest createPayoutRequest)
         {
             // Arrange
             var authApiMock = new Mock<IAuthApi>();
@@ -45,7 +48,7 @@ namespace TrueLayer.Tests.Payouts
             var sut = new PayoutsApi(Mock.Of<IApiClient>(), authApiMock.Object, _trueLayerOptions);
 
             // Act
-            var actual = await sut.CreatePayout(CreatePayoutRequest(), "indepotency-key", CancellationToken.None);
+            var actual = await sut.CreatePayout(createPayoutRequest, "indepotency-key", CancellationToken.None);
 
             //Assert
             actual.ShouldNotBeNull();
@@ -74,16 +77,40 @@ namespace TrueLayer.Tests.Payouts
             actual.TraceId.ShouldBe("trace-id");
         }
 
-        private static CreatePayoutRequest CreatePayoutRequest()
-            => new CreatePayoutRequest(
-                "merchant-account-id",
-                100,
-                Currencies.GBP,
-                new Beneficiary.ExternalAccount(
+        private static CreatePayoutRequest CreatePayoutRequest(bool paymentSource = false)
+        {
+            OneOf<Beneficiary.PaymentSource, Beneficiary.ExternalAccount> beneficiary;
+
+            if (paymentSource)
+            {
+                beneficiary = new Beneficiary.PaymentSource(
+                    "payment-source-id",
+                    "user-id",
+                    "truelayer-dotnet"
+                );
+            }
+            else
+            {
+                beneficiary = new Beneficiary.ExternalAccount(
                     "Ms. Lucky",
                     "truelayer-dotnet",
                     new AccountIdentifier.Iban("GB33BUKB20201555555555")
-                )
+                );
+            }
+
+            return new CreatePayoutRequest(
+                "merchant-account-id",
+                100,
+                Currencies.GBP,
+                beneficiary
             );
+        }
+
+        public static IEnumerable<object[]> TestData =>
+            new List<object[]>
+            {
+                new object[] { CreatePayoutRequest() },
+                new object[] { CreatePayoutRequest(true) },
+            };
     }
 }
