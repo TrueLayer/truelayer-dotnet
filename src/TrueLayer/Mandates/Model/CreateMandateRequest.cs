729e7e1ddab8e6293ab90572d02d63034b62a647
TANDEM\ryan.palmer <ryan.palmer@tandem.co.uk>
2023-07-14 13:08:40 +0100

    Docs for the Mandate Detail variations.

--- src/TrueLayer/Mandates/Model/CreateMandateRequest.cs
+++ src/TrueLayer/Mandates/Model/CreateMandateRequest.cs
@@ -20,7 +20,7 @@ namespace TrueLayer.Mandates.Model
         /// Creates a new <see cref="CreateMandateRequest"/>
         /// </summary>
         /// <param name="Mandate">Either a Commercial or Sweeping mandate.>
-        /// <param name="Currency">Gets the three-letter ISO currency code</param>
+        /// <param name="Currency">Three-letter ISO currency code</param>
         /// <param name="User">Details of the end user who is making the payment. Whether or not these fields are required depends on whether you are using your own PISP licence (if you are, these fields are not required).</param>
         /// <param name="Constraints">Sets the limits for the payments that can be created by the mandate. If a payment is attempted that doesn't fit within these constraints it will fail.</param>
         /// <param name="Metadata">Optional field for adding custom key-value data to a resource. This object can contain a maximum of 10 key-value pairs, each with a key with a maximum length of 40 characters and a non-null value with a maximum length of 500 characters.</param>
