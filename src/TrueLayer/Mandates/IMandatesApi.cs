1efadac2c8f474f7ef616a151f21c357f8884a96
mohammedmiah99 <mohammedmiah1999@outlook.com>
2023-07-24 10:18:05 +0100

    Amend api docs

--- src/TrueLayer/Mandates/IMandatesApi.cs
+++ src/TrueLayer/Mandates/IMandatesApi.cs
@@ -13,13 +13,13 @@ namespace TrueLayer.Mandates
         /// <summary>
         /// Creates a new mandate
         /// </summary>
-        /// <param name="mandateRequest">The payment request details</param>
+        /// <param name="mandateRequest">The mandate request details</param>
         /// <param name="idempotencyKey">
         /// An idempotency key to allow safe retrying without the operation being performed multiple times.
         /// The value should be unique for each operation, e.g. a UUID, with the same key being sent on a retry of the same request.
         /// </param>
         /// <param name="cancellationToken">The cancellation token to cancel the operation</param>
-        /// <returns>An API response that includes details of the created payment if successful, otherwise problem details</returns>
+        /// <returns>An API response that includes details of the created mandate if successful, otherwise problem details</returns>
         Task<ApiResponse<CreateMandateResponse>> CreateMandate(
             CreateMandateRequest mandateRequest, string idempotencyKey, CancellationToken cancellationToken = default);
     }
