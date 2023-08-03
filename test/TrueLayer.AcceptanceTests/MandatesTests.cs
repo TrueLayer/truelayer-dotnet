9ac7dc48f6fe0f5ce7cde5acdb6c8341f463e779
mohammedmiah99 <mohammedmiah1999@outlook.com>
2023-08-03 16:21:56 +0100

    Amend tests for debugging

--- test/TrueLayer.AcceptanceTests/MandatesTests.cs
+++ test/TrueLayer.AcceptanceTests/MandatesTests.cs
@@ -11,6 +11,10 @@ using Xunit;
 namespace TrueLayer.AcceptanceTests
 {
     using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Options;
     using TrueLayer.Models;
     using ProviderUnion = OneOf<Payments.Model.Provider.UserSelected, Mandates.Model.Provider.Preselected>;
     using MandateUnion = OneOf<Mandate.VRPCommercialMandate, Mandate.VRPSweepingMandate>;
@@ -19,14 +23,20 @@ namespace TrueLayer.AcceptanceTests
         AccountIdentifier.Iban,
         AccountIdentifier.Bban,
         AccountIdentifier.Nrb>;
+    using MandateDetailUnion = OneOf<MandateDetail.AuthorizationRequiredMandateDetail, MandateDetail.AuthorizingMandateDetail, MandateDetail.AuthorizedMandateDetail, MandateDetail.FailedMandateDetail, MandateDetail.RevokedMandateDetail>;
+    using AuthorizationResponseUnion = OneOf<Models.AuthorisationFlowResponse.AuthorizationFlowAuthorizing, Models.AuthorisationFlowResponse.AuthorizationFlowAuthorizationFailed>;
 
     public class MandatesTests : IClassFixture<ApiTestFixture>
     {
         private readonly ApiTestFixture _fixture;
+        private TrueLayerOptions configuration;
+        public string RETURN_URI = "http://localhost:3000/callback";
+        public static string PROVIDER_ID = "ob-natwest-vrp-sandbox";
 
         public MandatesTests(ApiTestFixture fixture)
         {
             _fixture = fixture;
+            configuration = fixture.ServiceProvider.GetRequiredService<IOptions<TrueLayerOptions>>().Value;
         }
 
         private static CreateMandateRequest CreateTestMandateRequest(
@@ -49,7 +59,7 @@ namespace TrueLayer.AcceptanceTests
         private static IEnumerable<object[]> CreateTestMandateRequests()
         {
             var accountIdentifier = new AccountIdentifier.SortCodeAccountNumber("111111", "10001000");
-            yield return new object[]
+            /*yield return new object[]
             {
                 CreateTestMandateRequest(MandateUnion.FromT1(new Mandate.VRPSweepingMandate(
                     "sweeping",
@@ -74,12 +84,12 @@ namespace TrueLayer.AcceptanceTests
                         "external_account",
                         "My Bank Account",
                         AccountIdentifierUnion.FromT0(accountIdentifier))))),
-            };
+            };*/
             yield return new object[]
             {
                 CreateTestMandateRequest(MandateUnion.FromT1(new Mandate.VRPSweepingMandate(
                     "sweeping",
-                    ProviderUnion.FromT1(new Mandates.Model.Provider.Preselected("preselected", "ob-natwest-vrp-sandbox")),
+                    ProviderUnion.FromT1(new Mandates.Model.Provider.Preselected("preselected", PROVIDER_ID)),
                     new Mandates.Model.Beneficiary.ExternalAccount(
                         "external_account",
                         "My Bank Account",
@@ -89,7 +99,7 @@ namespace TrueLayer.AcceptanceTests
             {
                 CreateTestMandateRequest(MandateUnion.FromT0(new Mandate.VRPCommercialMandate(
                     "commercial",
-                    ProviderUnion.FromT1(new Mandates.Model.Provider.Preselected("preselected", "ob-natwest-vrp-sandbox")),
+                    ProviderUnion.FromT1(new Mandates.Model.Provider.Preselected("preselected", PROVIDER_ID)),
                     new Mandates.Model.Beneficiary.ExternalAccount(
                         "external_account",
                         "My Bank Account",
@@ -154,14 +164,50 @@ namespace TrueLayer.AcceptanceTests
             var mandateId = createResponse.Data!.Id;
             StartAuthorizationFlowRequest authorizationRequest = new(
                 new ProviderSelection(ConfigurationStatus.Supported),
-                new Redirect(new Uri("https://my-site.com/mandate-return")));
+                new Redirect(new Uri(RETURN_URI)));
             // Act
             var response = await _fixture.Client.Mandates.StartAuthorizationFlow(
                 mandateId, authorizationRequest, idempotencyKey: Guid.NewGuid().ToString());
+            await AuthorizeMandate(response);
+            var mandate = await WaitForMandateToBeAuthorized(mandateId);
 
             // Assert
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            mandate.AsT2.Status.ShouldBe("authorized");
             createResponse.StatusCode.ShouldBe(HttpStatusCode.Created);
+        }
+
+        private async Task AuthorizeMandate(AuthorizationResponseUnion authorizationFlowResponse)
+        {
+            var uri = authorizationFlowResponse.AsT0.AuthorizationFlow.Actions.Next.AsT4.Uri;
+            var client = new HttpClient();
+            var response = await client.GetAsync(uri);
+            var paymentsSpaRedirectUrl = response.Headers.Location;
+            bool isQuery = paymentsSpaRedirectUrl?.Query is not null;
+            var rawParameters = isQuery ? paymentsSpaRedirectUrl?.Query : paymentsSpaRedirectUrl?.Fragment;
+            var sanitizedParameters = rawParameters?.Replace("state=mandate-", "state=");
+            string jsonPayload = isQuery
+                ? "{\"query\": \"?" + sanitizedParameters + "\"}"
+                : "{\"fragment\": \"#" + sanitizedParameters + "\"}";
+            var baseUri = configuration.Payments?.Uri;
+            var submitProviderParamsResponse = await client.PostAsync(
+                $"{baseUri}/spa/payments-provider-return",
+                new StringContent(jsonPayload, Encoding.UTF8, "application/json"));
+            submitProviderParamsResponse.IsSuccessStatusCode.ShouldBeTrue();
+        }
+
+        private async Task<MandateDetailUnion> WaitForMandateToBeAuthorized(string mandateId)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                await Task.Delay(1000);
+                var mandate = await _fixture.Client.Mandates.GetMandate(mandateId, MandateType.sweeping);
+                if (mandate.Data.AsT2.Status == "authorized")
+                {
+                    return mandate;
+                }
+            }
+            return await _fixture.Client.Mandates.GetMandate(mandateId, MandateType.sweeping);
         }
 
         [Theory]
@@ -172,7 +218,12 @@ namespace TrueLayer.AcceptanceTests
             var createResponse = await _fixture.Client.Mandates.CreateMandate(
                 mandateRequest, idempotencyKey: Guid.NewGuid().ToString());
             var mandateId = createResponse.Data!.Id;
-            SubmitProviderSelectionRequest request = new("ob-natwest-vrp-sandbox");
+            SubmitProviderSelectionRequest request = new(PROVIDER_ID);
+            StartAuthorizationFlowRequest authorizationRequest = new(
+                new ProviderSelection(ConfigurationStatus.Supported),
+                new Redirect(new Uri(RETURN_URI)));
+            await _fixture.Client.Mandates.StartAuthorizationFlow(
+                mandateId, authorizationRequest, idempotencyKey: Guid.NewGuid().ToString());
             // Act
             var response = await _fixture.Client.Mandates.SubmitProviderSelection(
                 mandateId, request, idempotencyKey: Guid.NewGuid().ToString());
