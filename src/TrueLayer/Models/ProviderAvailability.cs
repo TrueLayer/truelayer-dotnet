1c340ef551dbe86e85e5e1db7f016e5b2d043f5b
Mauro Franchi <mauro.franchi@truelayer.com>
2024-07-01 08:45:56 +0200

    chore(io-20): removed ErrorRate from ProviderAvailability

--- src/TrueLayer/Models/ProviderAvailability.cs
+++ src/TrueLayer/Models/ProviderAvailability.cs
@@ -13,11 +13,9 @@ namespace TrueLayer.Models
     /// Provider Availability object
     /// </summary>
     /// <param name="RecommendedStatus"></param>
-    /// <param name="ErrorRate">A ratio between the number of provider errors and all requests for the provider.</param>
     /// <param name="UpdatedAt">The point in time when this data was collected. Value is in UTC.</param>
     public record ProviderAvailability(
         string RecommendedStatus,
-        float ErrorRate,
         DateTime UpdatedAt
     );
 }
