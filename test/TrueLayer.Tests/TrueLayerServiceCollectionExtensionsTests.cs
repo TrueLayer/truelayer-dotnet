4caedd474801cf89e1d5553a4574abaee8bff751
Mauro Franchi <mauro.franchi@truelayer.com>
2024-08-05 14:50:57 +0200

    chore(acl-177): amended test to validate client is created when privateKey is provided from settings

--- test/TrueLayer.Tests/TrueLayerServiceCollectionExtensionsTests.cs
+++ test/TrueLayer.Tests/TrueLayerServiceCollectionExtensionsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,10 +12,19 @@ namespace TrueLayer.Tests
         [Fact]
         public void Should_register_truelayer_client()
         {
+            const string privateKey = @"-----BEGIN EC PRIVATE KEY-----
+MIHcAgEBBEIALJ2sKM+8mVDfTIlk50rqB5lkxaLBt+OECvhXq3nEaB+V0nqljZ9c
+5aHRN3qqxMzNLvxFQ+4twifa4ezkMK2/j5WgBwYFK4EEACOhgYkDgYYABADmhZbj
+i8bgJRfMTdtzy+5VbS5ScMaKC1LQfhII+PTzGzOr+Ts7Qv8My5cmYU5qarGK3tWF
+c3VMlcFZw7Y0iLjxAQFPvHqJ9vn3xWp+d3JREU1vQJ9daXswwbcoer88o1oVFmFf
+WS1/11+TH1x/lgKckAws6sAzJLPtCUZLV4IZTb6ENg==
+-----END EC PRIVATE KEY-----";
             var configuration = new ConfigurationBuilder()
                 .AddInMemoryCollection(new KeyValuePair<string, string>[] {
                     new("TrueLayer:ClientId", "client_id"),
-                    new("TrueLayer:ClientSecret", "secret")
+                    new("TrueLayer:ClientSecret", "secret"),
+                    new("TrueLayer:Payments:SigningKey:KeyId", Guid.NewGuid().ToString()),
+                    new("TrueLayer:Payments:SigningKey:PrivateKey", privateKey),
                 })
                 .Build();
             var services = new ServiceCollection()
