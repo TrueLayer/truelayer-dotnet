45e2176f6000d4f9d6a2f003b42d88d4e753e374
Mauro Franchi <mauro.franchi@truelayer.com>
2024-09-10 12:52:24 +0200

    chore(acl-111): added new Uri extensions method and related tests

--- test/TrueLayer.Tests/Extensions/UriExtensionsTests.cs
+++ test/TrueLayer.Tests/Extensions/UriExtensionsTests.cs
@@ -12,7 +12,7 @@ namespace TrueLayer.Tests.Extensions
         [MemberData(nameof(UriTestData))]
         public void Append_Returns_Expected_Uri(
             Uri baseUri,
-            string[] segments,
+            string?[] segments,
             Uri expectedUri)
         {
             // Act
@@ -20,6 +20,81 @@ namespace TrueLayer.Tests.Extensions
 
             // Assert
             actual.ShouldBe(expectedUri);
+        }
+
+        [Theory]
+        [MemberData(nameof(QueryParametersTestData))]
+        public void AppendQueryParameters_Returns_Expected_Uri(
+            Uri baseUri,
+            IDictionary<string, string?> queryParams,
+            Uri expectedUri)
+        {
+            // Act
+            var actual = baseUri.AppendQueryParameters(queryParams);
+
+            // Assert
+            actual.ShouldBe(expectedUri);
+        }
+
+        public static IEnumerable<object?[]> QueryParametersTestData()
+        {
+            string baseUrl = "http://test.foo.com/";
+            Uri baseUri = new(baseUrl);
+
+            yield return new object?[]
+            {
+                baseUri,
+                new Dictionary<string, string?> { ["param1"] = "value1" },
+                new Uri($"{baseUrl}?param1=value1")
+            };
+            yield return new object?[]
+            {
+                baseUri,
+                new Dictionary<string, string?> { ["param1"] = "value1", ["param2"] = "value2" },
+                new Uri($"{baseUrl}?param1=value1&param2=value2")
+            };
+            yield return new object?[]
+            {
+                baseUri,
+                new Dictionary<string, string?> { ["param1"] = null, ["param2"] = "value2" },
+                new Uri($"{baseUrl}?param2=value2")
+            };
+            yield return new object?[]
+            {
+                baseUri,
+                new Dictionary<string, string?> { ["param1"] = null, ["param2"] = "" },
+                new Uri($"{baseUrl}?param2=")
+            };
+            yield return new object?[]
+            {
+                baseUri,
+                new Dictionary<string, string?> { ["param1"] = null, ["param2"] = "  " },
+                new Uri($"{baseUrl}?param2=")
+            };
+            yield return new object?[]
+            {
+                baseUri,
+                new Dictionary<string, string?> { ["param1"] = "  ", ["param2"] = "value2" },
+                new Uri($"{baseUrl}?param1=&param2=value2")
+            };
+            yield return new object?[]
+            {
+                baseUri,
+                new Dictionary<string, string?>(),
+                new Uri(baseUrl)
+            };
+            yield return new object?[]
+            {
+                baseUri,
+                null,
+                new Uri(baseUrl)
+            };
+            yield return new object[]
+            {
+                new Uri($"{baseUrl}?existing=param"),
+                new Dictionary<string, string?> { ["param1"] = "value1"  },
+                new Uri($"{baseUrl}?existing=param&param1=value1")
+            };
         }
 
         public static IEnumerable<object[]> UriTestData()
@@ -41,6 +116,24 @@ namespace TrueLayer.Tests.Extensions
                 new Uri("http://test.foo.test"),
                 new[] { "/test" },
                 new Uri("http://test.foo.test/test"),
+            };
+            yield return new object[]
+            {
+                new Uri("http://test.foo.test"),
+                new string?[] { null },
+                new Uri("http://test.foo.test"),
+            };
+            yield return new object[]
+            {
+                new Uri("http://test.foo.test"),
+                new[] { "", "foo", "  ", null, "test" },
+                new Uri("http://test.foo.test/foo/test"),
+            };
+            yield return new object[]
+            {
+                new Uri("http://test.foo.test"),
+                new[] { "", " " },
+                new Uri("http://test.foo.test"),
             };
         }
     }
